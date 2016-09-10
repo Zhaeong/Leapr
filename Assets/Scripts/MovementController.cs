4bbf8cafb8b0f0b3f27632e15f4af24e5537eebc
Zhaeong <owen_zzz@hotmail.com>
2016-09-09 22:04:11 -0400

    1st commit

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+    public GameObject playerObject;
+
+    Camera MainCam;
+	// Use this for initialization
+	void Start () {
+        MainCam = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        foreach(Touch touch in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("regist");
+                playerObject.transform.position = MainCam.ScreenToWorldPoint(touch.position);
+            }
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            playerObject.transform.position = MainCam.ScreenToWorldPoint(Input.mousePosition);
+        }
+        
+
+    }
+}
