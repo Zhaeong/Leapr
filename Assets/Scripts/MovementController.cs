f926b4be33c5e9f6a5c0f91bdd5bd95aaa14c8e7
Zhaeong <owen_zzz@hotmail.com>
2016-09-09 22:16:50 -0400

    added some controls

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -6,26 +6,36 @@ public class MovementController : MonoBehaviour {
     public GameObject playerObject;
 
     Camera MainCam;
-	// Use this for initialization
-	void Start () {
+
+    RaycastHit hitInfo;
+
+    // Use this for initialization
+    void Start () {
         MainCam = Camera.main;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        foreach(Touch touch in Input.touches)
-        {
-            if(touch.phase == TouchPhase.Began)
-            {
-                Debug.Log("regist");
-                playerObject.transform.position = MainCam.ScreenToWorldPoint(touch.position);
-            }
-        }
+        //foreach(Touch touch in Input.touches)
+        //{
+        //    if(touch.phase == TouchPhase.Began)
+        //    {
+        //        Debug.Log("regist");
+        //        playerObject.transform.position = MainCam.ScreenToWorldPoint(touch.position);
+        //    }
+        //}
 
         if(Input.GetMouseButton(0))
         {
-            playerObject.transform.position = MainCam.ScreenToWorldPoint(Input.mousePosition);
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo, 100))
+            {
+                //playerObject.transform.position = MainCam.ScreenToWorldPoint(Input.mousePosition);
+                playerObject.transform.position = hitInfo.transform.position;
+
+            }
         }
         
 
